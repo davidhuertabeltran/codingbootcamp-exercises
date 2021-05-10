1f71a3683ebde5290cd78d33cdf65c11c9f7d074
David <huerta.beltran.david@gmail.com>
2021-05-10 17:15:52 +0200

    javascript exercises added

---
+++ day11/javascript to html/script.js
@@ -0,0 +1,61 @@
+//Birth Year
+
+    //let prompt = Number(window.prompt("In which year did your born?", ""));
+
+    //let age = 2021 - prompt;
+    //console.log(age);
+
+//Velocity
+
+    // let mph = 1.609344;
+
+    // let kmh = 1 * mph;
+
+    // console.log(Math.round(kmh));
+
+    // let velocity = Number(window.prompt("Please write velocity in miles", ""));
+    
+    // console.log(kmh * velocity);
+
+// Curency Exchange
+
+    // let crown = 25.695;
+
+    // let euros = Number(window.prompt("Enter an amount in Euros", "")) * crown;
+
+    // console.log(Math.round(euros));
+
+// SMS splitting
+
+    // let message = window.prompt("Enter a text message", "");
+
+    // console.log(message.length);
+
+    // let cell = message.length/160;
+
+    // console.log(Math.ceil(cell));
+
+// Celsius to Farenheit
+    
+
+    //     let temperature = window.prompt("Enter a Farenehit temperature");
+
+    //    temperature = Number(temperature.replace(',', '.'));
+
+    //     console.log(temperature);
+
+    //     let celsius = ((temperature + 40) / 1.8) - 40;
+    //     console.log(celsius);
+
+// Dice Rolls
+
+    // let dice = Math.floor(Math.random() * 6 + 1);
+                      
+    // console.log(dice);      
+
+
+    
+    
+
+
+

ca823d5ee1062630749888c9f6fea9091d738d9d
David <huerta.beltran.david@gmail.com>
2021-05-20 11:05:32 +0200

    day19 morning workout final

--- day19/morning workout/index.js
+++ day19/morning workout/index.js
@@ -1,15 +1,72 @@
 'use strict'
 
-let starsAll = document.querySelector('.rating__stars');
-let starOn = document.querySelector('.rating__star--on');
-let starOff = starOn.classList.remove('.rating__star--on');
+// let starsAll = document.querySelectorAll('.rating__star');
 
-const changeStars = () => {
-    for(let i = 0; i < starsAll.length; i++) {
-        console.log(starOff);
-        return starOff;
+// let rating = document.querySelector('.rating__value');
 
-    }
+// for(let i = 0; i< starsAll.length; i++) {
+
+//     starsAll[i].addEventListener('click', (event) => {
+//         for(let j = 0; j < starsAll.length; j++) {
+//             starsAll[j].setAttribute("id", j);
+//             console.log(starsAll[j]);
+//         }
+//        let currentId = event.target.id;
+//         for(let j = 0; j < starsAll.length; j++) {
+//             if (starsAll[j].getAttribute("id") < currentId) {
+//                 starsAll[j].classList.add('rating__star--on');
+//             } else {
+//                 starsAll[j].classList.remove('rating__star--on');
+//             }
+//         }
+//     })
+// }
+
+let currentValue = 3;
+
+const counter = document.querySelector(".rating__value");
+const stars = document.querySelectorAll(".rating__star");
+
+// This is forEach loop
+// stars.forEach((star, index) => {
+//     star.addEventListener('click', () => {
+//         handleClick(index);
+//     })
+// })
+
+for(let i = 0; i < stars.length; i++) {
+    stars[i].addEventListener('click', () => {
+        handleClick(i);
+    })
+
 }
 
-// document.querySelector('.rating__stars').addEventListener('click', changeStars);
+
+const handleClick = (index) => {
+    currentValue = index + 1;
+
+    for (let i = 0; i < stars.length; i++) {
+        const star = stars[i];
+        if (i < currentValue) {
+            star.classList.add('rating__star--on');
+        } else {
+            star.classList.remove('rating__star--on');
+        }
+    }
+
+    //This is forEach loop
+    // stars.forEach((star, index) => {
+    //     if (index < currentValue) {
+    //         star.classList.add('rating__star--on');
+    //     } else {
+    //         star.classList.remove('rating__star--on');
+    //     }
+    // })
+
+    counter.textContent = currentValue;
+}
+
+
+
+
+
